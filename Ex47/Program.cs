d229449c1ac7e848eb2f51783328c80063b90be0
Ekaterina <katya.kinal@gmail.com>
2022-08-26 13:00:57 +0300

    Ex47

---
+++ Ex47/Program.cs
@@ -0,0 +1,14 @@
+﻿double [,] array = new double [3, 4];
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Convert.ToDouble(new Random().Next(-10, 11) / 10.0);
+        Console.Write(array[i, j] + "\t");
+
+    }
+    Console.WriteLine();
+}
+
+
